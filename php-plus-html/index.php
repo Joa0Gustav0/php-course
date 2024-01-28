3eac0b6e168a7ce92d4c02f9e683ea1140dae362
Joao <dev.joaogustavo.dev@gmail.com>
2024-01-28 09:22:03 -0300

    studying php | embbeding php into html

---
+++ php-plus-html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Embed PHP in HTML</title>
+</head>
+<body>
+  <h1>
+    <?php echo "Hey! This is how to<br/>embed PHP into HTML! 😎" ?>
+  </h1>
+  <p><?= "I am confident! 🧠" ?></p>
+</body>
+</html>

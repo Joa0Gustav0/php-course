3a4b165e32d5ab6f7337b77efcc504ad08ffad3f
Joao <dev.joaogustavo.dev@gmail.com>
2024-01-28 09:40:08 -0300

    studying php | constants

---
+++ consts/index.php
@@ -0,0 +1,12 @@
+<?php
+//Define Function
+define("CLIENT_ID", 123213);
+
+if (defined("CLIENT_ID") === true) {
+  echo CLIENT_ID;
+}
+
+//Const Keyword
+const _CLIENT_ID = 12321332543;
+
+echo _CLIENT_ID;

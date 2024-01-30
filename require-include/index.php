2030893c215fcc4bddf9150ca4eb4f37e40743fc
Joao <dev.joaogustavo.dev@gmail.com>
2024-01-30 17:16:16 -0300

    studying php | require and include statements

---
+++ require-include/index.php
@@ -0,0 +1,15 @@
+<?php
+#require
+echo "Studying PHP 🐘<br/>";
+require("module.php");
+
+echo "<br/>";
+#require_once
+require_once("module.php");
+
+echo "<br/>";
+#include
+include "module.php";
+
+#include_once
+include_once "module.php";

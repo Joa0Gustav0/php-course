cb07ef6ec69ab928102cd95bebd9e83cd79e31db
Joao <dev.joaogustavo.dev@gmail.com>
2024-02-02 20:11:10 -0300

    studying php | static methods and properties and destructor method

--- oop/classes-and-objects/index.php
+++ oop/classes-and-objects/index.php
@@ -5,28 +5,46 @@ class User {
   #properties 🔒
   private $username;
   private $userID;
+  public static $quantityOfUsers;
 
   #constructor 🔨
   function __construct(string $_username, int $_userID) {
     $this->username = $_username;
     $this->userID = $_userID;
+    self::$quantityOfUsers++;
+  }
+
+  #destructor 🧨
+  function __destruct() {
+    self::$quantityOfUsers--;
   }
 
   #method 🕓
   public function getData(string $target) {
     switch($target) {
       case "username":
-        echo $this->username;
+        echo $this->username."<br/>";
         break;
       case "userID":
-        echo $this->userID;
+        echo $this->userID."<br/>";
         break;
     }
 
     return $this;
   }
+
+  #static method
+  public static function getQuantityOfUsers() {
+    return "<p>The quantity of users is: ". self::$quantityOfUsers;
+  }
 }
 
+#Method Chaining ⛓
 $newUser = new User("Gabriel", 1234);
-#Method Chaining
 $newUser->getData("username")->getData("userID");
+
+$anotherNewUser = new User("Paulo", 1987);
+$anotherNewUser->getData("username")->getData("userID");
+unset($anotherNewUser);
+
+echo User::getQuantityOfUsers();
